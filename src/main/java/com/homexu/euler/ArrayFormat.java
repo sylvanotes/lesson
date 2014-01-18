6ab6722ce9236f341b8382af8b9e20b10394edc6
Camilla <Camilla@192.168.0.134>
2014-01-17 20:06:12 -0500

    Formmating Arrays

---
+++ src/main/java/com/homexu/euler/ArrayFormat.java
@@ -0,0 +1,19 @@
+package com.homexu.euler;
+
+public class ArrayFormat {
+	
+	public static void display(int[] newArray)
+	{
+		System.out.print("(");
+		for(int i = 0; i<newArray.length-1; i++)
+			System.out.print(newArray[i] + ", ");
+		System.out.print(newArray[newArray.length-1] + ")");
+	}
+	
+	public static void main(String[] args)
+	{
+		int x[] = {34, 56, 78, 49, 60, 2, 38, 10};
+		display(x);
+	}
+	
+}
---
+++ src/test/java/com/homexu/euler/ArrayFormat_test.java
@@ -0,0 +1,13 @@
+package com.homexu.euler;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class ArrayFormat_test {
+
+	@Test
+	public void test() {
+		//Sorry I don't know how to write the test class for this
+	}
+}
