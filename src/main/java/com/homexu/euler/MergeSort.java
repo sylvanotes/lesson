03324dffdf05020e10d99ebf0295ea390b84de37
Camilla <Camilla@192.168.0.114>
2014-03-01 23:43:12 -0500

    MergeSort w/o start and end

--- src/main/java/com/homexu/euler/MergeSort.java
+++ src/main/java/com/homexu/euler/MergeSort.java
@@ -2,24 +2,10 @@ package com.homexu.euler;
 
 public class MergeSort {
 	
-	public static void mSort(int[] input)
+/*	public static void mSort(int[] input)
 	{
 		int size = 1;
-		if(input.length%2==0)
-		{
-			for(int i = 0; i<input.length; i+= (2*size))
-			{
-				if(input[i+1] < input[i])
-				{
-					int temp = input[i];
-					input[i] = input[i+1];
-					input[i+1] = temp;
-				}
-			}
-		}
-		
-		if(input.length%2==1)
-		{
+
 			for(int i = 0; i<input.length-1; i+=(2*size))
 			{
 				if(input[i+1] < input[i])
@@ -29,8 +15,48 @@ public class MergeSort {
 					input[i+1] = temp;
 				}
 			}
-		}
 
+	}*/
+	
+	public static void sortArray(int[] input)
+	{
+		int[] temp = new int[input.length];
+		int first = 0;
+		int half = input.length/2;
+		int second = half;
+		
+		for(int x = 0; x<temp.length; x++){
+			if(input[first]<input[second]){
+				temp[x] = input[first];
+				first++;
+			}
+			
+			else
+			{
+				temp[x] = input[second];
+				second++;
+			}
+			
+			if(first==half||second==input.length)
+				break;
+		}
+		
+		for(int x = first+(second-half); x<temp.length; x++){
+			if(first<half)
+			{
+				temp[x] = input[first];
+				first++;
+			}
+			
+			else
+			{
+				temp[x]=input[second];
+				second++;
+			}
+		}
+		
+		for(int i = 0; i<temp.length; i++)
+			input[i] = temp[i];
 	}
 	
 	public static String display(int[] newArray)
--- src/main/java/com/homexu/euler/MergeSort_test.java
+++ src/main/java/com/homexu/euler/MergeSort_test.java
@@ -6,14 +6,32 @@ import org.junit.Test;
 
 
 public class MergeSort_test {
-	@Test
+	/*@Test
 	
-	public void test()
+	public void test_Random()
 	{
 		int[] toSort = {4, 8, 5, 2, 7, 1, 6, 9, 0, 11, 3};
-		MergeSort.mSort(toSort);
+		MergeSort.sortArray(toSort);
 		String x = MergeSort.display(toSort);
 		assertThat(x, equalTo("(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11)"));
+	}*/
+	
+	@Test
+	public void test_endsAtBoundary()
+	{
+		int[] toSort = {7, 8, 9, 1, 2, 3, 4};
+		MergeSort.sortArray(toSort);;
+		String x = MergeSort.display(toSort);
+		assertThat(x, equalTo("(1, 2, 3, 4, 7, 8, 9)"));
+	}
+	
+	@Test
+	public void test_doesNotEndAtBoundary()
+	{
+		int[] toSort = {1, 3, 4, 8, 9, 2, 2, 5, 6, 7};
+		MergeSort.sortArray(toSort);
+		String x = MergeSort.display(toSort);
+		assertThat(x, equalTo("(1, 2, 2, 3, 4, 5, 6, 7, 8, 9)"));
 	}
 	
 }
